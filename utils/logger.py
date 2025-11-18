44b28287b9e578ede970b213314c50c8081d38aa
Gadangadang <sakarias.frette@outlook.com>
2025-11-18 22:57:44 +0100

    changed the code to pep8 format with black

--- utils/logger.py
+++ utils/logger.py
@@ -1,8 +1,11 @@
-import pandas as pd  
+import pandas as pd
 import os
-from datetime import date      
+from datetime import date
 
-def log_daily_result(df:pd.DataFrame, ticker:str, log_path:str="logs/daily_log.csv"):
+
+def log_daily_result(
+    df: pd.DataFrame, ticker: str, log_path: str = "logs/daily_log.csv"
+):
     """_summary_
 
     Args:
@@ -10,7 +13,7 @@ def log_daily_result(df:pd.DataFrame, ticker:str, log_path:str="logs/daily_log.c
         ticker (str): _description_
         log_path (str, optional): _description_. Defaults to "logs/daily_log.csv".
     """
-   
+
     latest = df.iloc[-1]
     log_entry = {
         "Date": latest.name.date() if hasattr(latest.name, "date") else date.today(),
@@ -19,14 +22,14 @@ def log_daily_result(df:pd.DataFrame, ticker:str, log_path:str="logs/daily_log.c
         "Position": latest.get("Position", None),
         "Return": latest.get("Return", None),
         "StrategyReturn": latest.get("Strategy", None),
-        "CumulativeReturn": df["Strategy"].cumsum().iloc[-1]
+        "CumulativeReturn": df["Strategy"].cumsum().iloc[-1],
     }
-    
+
     if os.path.exists(log_path):
         log_df = pd.read_csv(log_path)
     else:
         log_df = pd.DataFrame(columns=log_entry.keys())
-        
+
     log_df = pd.concat([log_df, pd.DataFrame([log_entry])], ignore_index=True)
     log_df.to_csv(log_path, index=False)
     print(f"Logged result {ticker} on {log_entry.get('Date')}")
